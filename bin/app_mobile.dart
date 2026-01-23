f4c4be09a212c0a65a153551cc07c043c0bc48d5
AntoineBrosseau <antoine.brosseau@edu.ece.fr>
2026-01-23 18:52:16 +0100

    ajout

---
+++ bin/app_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:app_mobile/app_mobile.dart' as app_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app_mobile.calculate()}!');
+}
---
+++ test/app_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_mobile/app_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
